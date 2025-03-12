d2aa5d97541716c2c6cdea60c5665599a4940ed3
Azizbek <qodiraliyevazizbekpdp@gmail.com>
2025-03-12 16:56:49 +0500

    Loyihani yangiladim

--- src/main/urls.py
+++ src/main/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('', main),
-    path('', maind)
+    path('index-2.html', main),
+    path('index-3.html', maind),
+    path('category.html', mains),
+    path('adlistinglist.html', mainq),
+    path('adlistinggrid.html', mainw),
+    path('ads-details.html', maine),
+
 ]
 
--- src/main/views.py
+++ src/main/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
 from category.models import *
+
+
 def main(request):
     gb = Category.objects.all()
-    return render(request, 'index-2.html',{'gb': gb})
+    return render(request, 'index-2.html', {'gb': gb})
+
+
 def maind(request):
     gb = Category.objects.all()
-    return render(request, 'index-3.html',{'gb': gb})
+    return render(request, 'index-3.html', {'gb': gb})
+
+
+def mains(request):
+    gb = Category.objects.all()
+    return render(request, 'category.html', {'gb': gb})
+
+def mainq(request):
+    gb = Category.objects.all()
+    return render(request, 'adlistinglist.html', {'gb': gb})
+
+def mainw(request):
+    gb = Category.objects.all()
+    return render(request, 'adlistinggrid.html', {'gb': gb})
+
+def maine(request):
+    gb = Category.objects.all()
+    return render(request, 'ads-details.html', {'gb': gb})
+def product_detail(request, pk):
+    return render(request)

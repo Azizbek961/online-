fd06b754899b309189ca51c9af287fab93ce3308
Azizbek <qodiraliyevazizbekpdp@gmail.com>
2025-04-25 16:38:20 +0500

    first commit

--- blog/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import BLog, BlogComment
-
-admin.site.register(BLog)
-admin.site.register(BlogComment)
--- region/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Region(models.Model):
-    name = models.CharField(max_length=40, null=False, blank=False)
-    sorting = models.SmallIntegerField(null=False, blank=False, unique=True)
-
-    def __str__(self):
-        return self.name
